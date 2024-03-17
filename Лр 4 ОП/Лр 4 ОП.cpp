db2e4f901b8cd799919313de5eecb4fb2f7f1e00
Rauf <www.rauf09@gmail.com>
2024-03-17 15:57:16 +0300

    Добавьте файлы проекта.

---
+++ Лр 4 ОП/Лр 4 ОП.cpp
@@ -0,0 +1,43 @@
+﻿/* Дана строка. Слово есть любая последовательность букв, разделенных пробелом. Удалить в этой строке все слова, начинающиеся на заданную букву без
+учета регистра этой буквы.      
+ВХОД------ "Вас встретил я случайно",       'в'
+ВЫход ------- "я  случайно"
+
+*/
+
+
+
+#include <iostream>
+#include <string>
+
+std::string DEL_LETTER(std::string stroka, char letter) {
+	int i = 0;
+	while (stroka[i] != '\0') {
+		if (stroka[i] == ' ') {
+			++i;
+			continue;
+		}
+		if (stroka[i] == letter || stroka[i] == letter - 32 || stroka[i] == letter + 32) {
+			while (stroka[i] != ' ' && stroka[i] != '\0') {
+				++i;
+			}
+		}
+	}
+
+}
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	char letter;
+	std::string stroka;
+
+	std::cout << "Введите строку: " << std::endl;
+	std::getline(std::cin, stroka);
+
+	std::cout << "Введите букву, благодаря которой исчезнут слова, начинающиеся с этой буквы: " << std::endl;
+	std::cin >> letter;
+
+
+	return 0;
+}
